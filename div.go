55d2f5be991b6cc3d9d70b25cbcba2d78829abe9
Ravuri Sai Ram Akhil <rakhil@rocketsoftware.com>
2021-03-02 13:44:24 +0530

    Final with v1.0,0

--- div.go
+++ div.go
@@ -3,7 +3,6 @@ package cal
 import (
 	"fmt"
 	"time"
-	_ "github.com/ibmdb/go_ibm_db"
 )
 
 //Div is divide 2 float values
--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/akhilravuri1/calculator
 
 go 1.16
-
-require github.com/ibmdb/go_ibm_db v0.3.0
